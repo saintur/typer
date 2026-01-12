f4a7f2bcd2e66e2b755a01236fbd025f25deb3d6
esaintor <esaintor@gmail.com>
2026-01-12 16:29:08 -0600

    feat: both hand with each langs

--- src/app/components/left-hand/left-hand.ts
+++ src/app/components/left-hand/left-hand.ts
@@ -8,15 +8,48 @@ import {Component, Input} from '@angular/core';
 })
 export class LeftHand  {
   @Input() letter: string = 'Д';
+  @Input() language: string = 'mn';
   fingers = {
     thumb: ' ',
-    index: 'r,f,v,t,g,b,ж,ө,с,э,а,с,м^,%,:,.',
-    middle: 'e,d,c,у,б,ё,$,#,₮"',
-    ring: 'w,s,x,ц,ы,ч,@,-,№',
-    pinkie: 'q,a,z,фй,я,!,~,[,shift',
+    index: {
+      mn: 'ж,ө,с,э,а,с,м,:,.,5,6',
+      en: 'r,f,v,t,g,v,b,^,%,5,6'
+    },
+    middle: {
+      mn: 'у,б,ё,₮,",4',
+      en: 'e,d,c,$,#,4'
+    },
+    ring: {
+      mn: 'ц,ы,ч,-,2,3',
+      en: 'w,s,x,@,2,3',
+    },
+    pinkie: {
+      mn: 'ф,й,я,],[,1',
+      en: 'q,a,z,!,~,1',
+    }
   }
 
   get isUpperCase() {
     return (this.letter.match(/[A-ZА-ЯӨҮ]/g) || []).length > 0 || this.letter === ' ';
+  }
+
+  get index() {
+    // @ts-ignore
+    return this.fingers.index[this.language];
+  }
+
+  get middle() {
+    // @ts-ignore
+    return this.fingers.middle[this.language];
+  }
+
+  get ring() {
+    // @ts-ignore
+    return this.fingers.ring[this.language];
+  }
+
+  get pinkie() {
+    // @ts-ignore
+    return this.fingers.pinkie[this.language];
   }
 }
--- src/app/components/right-hand/right-hand.ts
+++ src/app/components/right-hand/right-hand.ts
@@ -8,15 +8,48 @@ import {Component, Input} from '@angular/core';
 })
 export class RightHand {
   @Input() letter: string = 'Д';
+  @Input() language: string = 'mn';
   fingers = {
     thumb: ' ',
-    index: 'y,h,b,n,u,j,m,н,х,и,м,г,р,т,^,&,.,_',
-    middle: 'i,k,\,,ш,о,ь,*,(,\,,%',
-    ring: 'o,l,.,ү,л,в,),_,?',
-    pinkie: 'p,;,/,\',[,],з,д,ю,к,п,ъ,shift,_,+,е,щ',
+    index: {
+      mn: 'н,х,и,м,г,р,т,.,_,7,6',
+      en: 'y,h,b,n,u,j,m,^,&,7,6'
+    },
+    middle: {
+      mn: 'ш,о,ь,\,,%,8,9',
+      en: 'i,k,\,,*,(,8,9'
+    },
+    ring: {
+      mn: 'ү,л,в,?,0',
+      en: 'o,l,.,),0'
+    },
+    pinkie: {
+      mn: 'з,д,ю,к,п,ъ,е,щ',
+      en: 'p,;,/,\',[,],_,+'
+    },
   }
 
   get isUpperCase() {
     return (this.letter.match(/[A-ZА-ЯӨҮ]/g) || []).length > 0 || this.letter === ' ';
+  }
+
+  get index() {
+    // @ts-ignore
+    return this.fingers.index[this.language];
+  }
+
+  get middle() {
+    // @ts-ignore
+    return this.fingers.middle[this.language];
+  }
+
+  get ring() {
+    // @ts-ignore
+    return this.fingers.ring[this.language];
+  }
+
+  get pinkie() {
+    // @ts-ignore
+    return this.fingers.pinkie[this.language];
   }
 }
