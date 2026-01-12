feb9968cde2562797c0fac6ec2c81f6684c220bd
esaintor <esaintor@gmail.com>
2026-01-12 16:41:11 -0600

    feat: small adjustment

--- src/app/components/composer/composer.ts
+++ src/app/components/composer/composer.ts
@@ -39,7 +39,7 @@ export class Composer implements OnChanges, AfterViewInit, OnInit, OnDestroy {
 
   previous: string = ''
   current: string = '';
-  @Input() original: string = '1234567890-=Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
+  @Input() original: string = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
   record: string = '';
   backspaced: number = 0;
 
