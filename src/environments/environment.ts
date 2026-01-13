fe4ec15f38eb2e1629a6633d0001979f02934a34
Sainzaya Batkhuu <sainzaya.b@gmail.com>
2026-01-13 22:44:58 +0800

    insert env and api services

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  api_url: 'http://18.140.81.67:8080/api/v1',
+  socket_url: 'http://18.140.81.67:8080/',
+  recaptcha: {
+    siteKey: '6LewJpYdAAAAALl1UtU0pd-Nso_DhedaDF0eeyt2',
+  }
+};
