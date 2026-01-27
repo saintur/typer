9fbf94e52a46bb83a67be9a21f6b847dafc186e0
esaintor <esaintor@gmail.com>
2026-01-26 19:09:14 -0600

    feat: api bicheech railway app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  api_url: 'https://api.bicheech.mn/api',
-  socket_url: 'https://api.bicheech.mn/',
+  api_url: 'https://bicheech-api.up.railway.app/api',
+  socket_url: 'https://bicheech-api.up.railway.app/',
   recaptcha: {
     siteKey: '0x4AAAAAACOi9yKlutlvVTeB',
   }
